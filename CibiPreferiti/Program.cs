fd3159bd8f726ab56e92828978beea34aae3fa23
alexpagani <alexpaganibr@gmail.com>
2022-04-01 14:27:09 +0200

    Aggiunta Cibi preferiti e stampa a video dei vari elementi

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿//dichiarazione array cibi preferiti dal migliore al meno peggio
+
+string [] cibiPreferiti = { "Coxinhas", "Carbonara", "Tiramisù", "Mango" ,"Spaghetti aglio olio e peperoncino", "Papaya", "Anacardi", "Cheesecake", "Crapfen", "Guava" };
+
+//Stampo a video quanti sono i cibi preferiti
+Console.WriteLine("Ci sono un totale di " + cibiPreferiti.Length + " nella tua lista di cibi preferiti");
+
+
+Console.WriteLine("Ecco la lista dei tuoi cibi preferiti");
+
+//Stampo gli elementi della lista
+for (int i = 0; i < cibiPreferiti.Length; i++) {
+    Console.WriteLine("- " + cibiPreferiti[i]);
+}
